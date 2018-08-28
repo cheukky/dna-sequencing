2b974cb340e3ce9d6a5a1078718615e89f88f294
ctt43 <ctt43@cornell.edu>
2018-08-28 00:25:15 -0400

    Some tests and 'A' functionality complete

--- dna_sequencing.py
+++ dna_sequencing.py
@@ -1,3 +1,91 @@
+import sys
+
+
 def dna_sequencing(sequence):
+    '''This function calculates the statistics of the string and returns a
+    dictionary of the output.'''
     result = dict()
+
+    a_counter = 0
+    c_counter = 0
+    g_counter = 0
+    t_counter = 0
+    other_counter = 0
+
+    a_consec = 0
+    a_maxconsec = 0
+    c_consec = 0
+    c_maxconsec = 0
+    g_consec = 0
+    g_maxconsec = 0
+    t_consec = 0
+    t_maxconsec = 0
+
+    a_percent = 0.0
+    c_percent = 0.0
+    g_percent = 0.0
+    t_percent = 0.0
+    other_percent = 0.0
+
+    for letter in sequence:
+        if letter == "A" or letter == 'a':
+            a_counter += 1
+            a_consec += 1
+
+            if(c_consec > c_maxconsec):
+                c_maxconsec = c_consec
+            c_consec = 0
+
+            if(g_consec > g_maxconsec):
+                g_maxconsec = g_consec
+            g_consec = 0
+
+            if(t_consec > t_maxconsec):
+                t_maxconsec = t_consec
+            t_consec = 0
+        else:
+            if(a_consec > a_maxconsec):
+                a_maxconsec = a_consec
+            a_consec = 0
+
+    # consecutive
+    if(a_consec > a_maxconsec):
+        a_maxconsec = a_consec
+    if(c_consec > c_maxconsec):
+        c_maxconsec = c_consec
+    if(g_consec > g_maxconsec):
+        g_maxconsec = g_consec
+    if(t_consec > t_maxconsec):
+        t_maxconsec = t_consec
+
+    # percentages
+    if len(sequence) > 0:
+        a_percent = a_counter/len(sequence)
+
+    result["A"] = a_counter
+    result["C"] = c_counter
+    result["G"] = g_counter
+    result["T"] = t_counter
+    result["Other"] = other_counter
+
+    result["Max consecutive As"] = a_maxconsec
+    result["Max consecutive Cs"] = c_maxconsec
+    result["Max consecutive Gs"] = g_maxconsec
+    result["Max consecutive Ts"] = t_maxconsec
+
+    result["Percentage of As"] = a_percent
+    result["Percentage of Cs"] = c_percent
+    result["Percentage of Gs"] = g_percent
+    result["Percentage of Ts"] = t_percent
+    result["Percentage of other characters"] = other_percent
+
+    print("Number of As: "+str(result["A"]))
+
+    print("Greatest number of consecutive As: " + str(result["Max consecutive As"]))
+
+    print("Percentage of As:"+str(result["Percentage of As"]))
+
     return result
+
+
+dna_sequencing(sys.argv[1])
--- test_dna_sequencing.py
+++ test_dna_sequencing.py
@@ -1,8 +1,72 @@
-import pytest
 from dna_sequencing import dna_sequencing
 
 
 class TestClass(object):
     def test_empty(self):
         res = dna_sequencing('')
-        assert res == {}
+        assert res == {'A': 0, 'C': 0, 'G': 0, 'Max consecutive As': 0,
+                       'Max consecutive Cs': 0, 'Max consecutive Gs': 0,
+                       'Max consecutive Ts': 0, 'Other': 0,
+                       'Percentage of As': 0.0, 'Percentage of Cs': 0.0,
+                       'Percentage of Gs': 0.0, 'Percentage of Ts': 0.0,
+                       'Percentage of other characters': 0.0, 'T': 0}
+
+    def test_a(self):
+        res = dna_sequencing("A")
+        assert res["A"] == 1
+
+    def test_as(self):
+        res = dna_sequencing("AAA")
+        assert res["A"] == 3
+
+    def test_more_as(self):
+        res = dna_sequencing("ACGTACGTACGT")
+        assert res["A"] == 3
+
+    def test_more_as_2(self):
+        res = dna_sequencing("AAAACCCCGGGGTTTT")
+        assert res["A"] == 4
+
+    def test_more_as_3(self):
+        res = dna_sequencing("AaaanCcccdGgggeTTtTf")
+        assert res["A"] == 4
+
+    def test_consec_a(self):
+        res = dna_sequencing("A")
+        assert res["Max consecutive As"] == 1
+
+    def test_consec_as(self):
+        res = dna_sequencing("AAA")
+        assert res["Max consecutive As"] == 3
+
+    def test_more_consec_as(self):
+        res = dna_sequencing("ACGTACGTACGT")
+        assert res["Max consecutive As"] == 1
+
+    def test_more_consec_as_2(self):
+        res = dna_sequencing("AAAACCCCGGGGTTTT")
+        assert res["Max consecutive As"] == 4
+
+    def test_more_consec_as_3(self):
+        res = dna_sequencing("AaaanCcccdGgggeTTtTf")
+        assert res["Max consecutive As"] == 4
+
+    def test_percent_a(self):
+        res = dna_sequencing("A")
+        assert res["Percentage of As"] == 1.0
+
+    def test_percent_as(self):
+        res = dna_sequencing("AAA")
+        assert res["Percentage of As"] == 1.0
+
+    def test_more_percent_as(self):
+        res = dna_sequencing("ACGTACGTACGT")
+        assert res["Percentage of As"] == 0.25
+
+    def test_more_percent_as_2(self):
+        res = dna_sequencing("AAAACCCCGGGGTTTT")
+        assert res["Percentage of As"] == 0.25
+
+    def test_more_percent_as_3(self):
+        res = dna_sequencing("AaaanCcccdGgggeTTtTf")
+        assert res["Percentage of As"] == 0.2
